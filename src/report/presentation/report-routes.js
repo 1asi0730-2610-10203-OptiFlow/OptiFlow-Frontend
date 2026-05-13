568253db5b0a9d9243f1474df127d2db870343fc
Patt <marianamorocho827@gmail.com>
2026-05-13 01:36:10 -0500

    feat(report): register routes and add translations for reports.

---
+++ src/report/presentation/report-routes.js
@@ -0,0 +1,10 @@
+import ReportView from './views/report-view.vue'
+
+export default [
+  {
+    path: '/reports',
+    name: 'reports',
+    component: ReportView,
+    meta: { title: 'Reports' }
+  }
+]
