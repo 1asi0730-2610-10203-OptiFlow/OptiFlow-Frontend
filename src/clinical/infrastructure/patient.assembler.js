4daf88c2cee129227f8898e4476917bc5510c40d
Fernando N. <nicholasbeckenbauer@gmail.com>
2026-06-16 17:48:53 -0500

    set assemblers in clinical

--- src/clinical/infrastructure/patient.assembler.js
+++ src/clinical/infrastructure/patient.assembler.js
@@ -4,14 +4,14 @@ import { PatientResource } from './patient.resource.js'
 export class PatientAssembler {
     static toEntityFromResource(resource) {
         return new Patient({
-            id:           resource.id ?? resource.patient_id ?? 0,
-            customerUuid: resource.customer_uuid ?? '',
-            firstName:    resource.first_name    ?? '',
-            lastName:     resource.last_name     ?? '',
-            dni:          resource.dni           ?? '',
-            phone:        resource.phone         ?? '',
-            email:        resource.email         ?? '',
-            birthDate:    resource.birth_date    ?? ''
+            id:           resource.id ?? 0,
+            customerUuid: '',                                              // ya no viene del backend
+            firstName:    resource.firstName    ?? resource.first_name    ?? '',
+            lastName:     resource.lastName     ?? resource.last_name     ?? '',
+            dni:          resource.dni          ?? '',
+            phone:        resource.phone        ?? '',
+            email:        resource.email        ?? '',
+            birthDate:    resource.birthDate    ?? resource.birth_date    ?? ''
         })
     }
 
@@ -21,14 +21,12 @@ export class PatientAssembler {
 
     static toResourceFromEntity(patient) {
         return new PatientResource({
-            patient_id:    patient.id,
-            customer_uuid: patient.customerUuid,
-            first_name:    patient.firstName,
-            last_name:     patient.lastName,
-            dni:           patient.dni,
-            phone:         patient.phone,
-            email:         patient.email,
-            birth_date:    patient.birthDate
+            first_name: patient.firstName,
+            last_name:  patient.lastName,
+            dni:        patient.dni,
+            phone:      patient.phone,
+            email:      patient.email,
+            birth_date: patient.birthDate
         })
     }
 }
