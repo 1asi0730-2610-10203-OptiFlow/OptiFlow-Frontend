f91ee4c6206cb61c3e154121a9445b5b1b6e2ffa
Fernando N. <nicholasbeckenbauer@gmail.com>
2026-06-17 22:33:16 -0500

    set prescription-api

--- src/clinical/infrastructure/prescription-api.js
+++ src/clinical/infrastructure/prescription-api.js
@@ -3,13 +3,21 @@ import { BaseEndpoint } from '../../shared/infrastructure/base-endpoint.js'
 
 export class PrescriptionApi extends BaseApi {
     #endpoint
+    #basePath
 
     constructor() {
         super()
-        this.#endpoint = new BaseEndpoint(this.http, import.meta.env.VITE_PRESCRIPTIONS_ENDPOINT_PATH)
+        this.#basePath = import.meta.env.VITE_PRESCRIPTIONS_ENDPOINT_PATH
+        this.#endpoint = new BaseEndpoint(this.http, this.#basePath)
     }
 
-    async getPrescriptions()                       { return await this.#endpoint.getAll() }
-    async getPrescriptionsByRecordId(recordId)     { return await this.#endpoint.getAllByParam('clinical_record_id', recordId) }
-    async createPrescription(resource)             { return await this.#endpoint.create(resource) }
+    async getPrescriptions()                   { return await this.#endpoint.getAll() }
+
+    // El backend usa /by-clinical-record/{recordId} en lugar de query params
+    async getPrescriptionsByRecordId(recordId) {
+        const response = await this.http.get(`${this.#basePath}/by-clinical-record/${recordId}`)
+        return response.data
+    }
+
+    async createPrescription(resource)         { return await this.#endpoint.create(resource) }
 }
